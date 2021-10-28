ff4bee7bc5503e7f6fcc73ae28654b53ba0e1a05
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-10-28 17:46:29 +0100

    IMS-34 #comment Update sql-Schema for testing purposes.

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -3,3 +3,5 @@ INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `items` (`item_name`, `cost`) VALUES ('Fifa22', 59.99);
 
 INSERT INTO `orders` (`fk_customer_id`) VALUES ('1');
+
+INSERT INTO `order_items` (`fk_order_id`, `fk_item_id`) VALUES (1, 1);
